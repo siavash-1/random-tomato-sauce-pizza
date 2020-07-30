d1b37d79a5e0643dbd8a5908e223643d63e6827e
siavash <69014041+siavash-1@users.noreply.github.com>
2020-07-30 22:34:18 +0200

    Update tomato-soup.py

--- tomato-soup.py
+++ tomato-soup.py
@@ -1,1 +1,25 @@
+##
+import random
+statist = 0
+tomatis = 2.7
+statistlist = []
+for x in range(1000):
+    for x in range(0,100):
+        numbers = []
+        for x in range(100):
+            numbers.append(random.randint(0,(9**999)))
+        
+        numbers = list(set(numbers)) 
+        random.shuffle(numbers)
+            
+        first_part = numbers[0:(int(((len(numbers))/tomatis)))]
+        second_part = numbers[(int(((len(numbers))/tomatis))):]
+        if max(first_part) == max(numbers):
+            statist = statist+1
+            
+    statistlist.append(statist)
+    statist = 0
+    
+print(statistlist)
+print(sum(statistlist)/len(statistlist))
 
